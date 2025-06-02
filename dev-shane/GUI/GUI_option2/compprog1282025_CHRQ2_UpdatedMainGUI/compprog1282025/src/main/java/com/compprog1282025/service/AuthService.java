a418f423d484dc8265176959c91952c313fb7f8f
lrsrivera <lr.srivera@mmdc.mcl.edu.ph>
2025-06-02 21:11:30 +0800

    Delete dev-shane/GUI/GUI_option2/compprog1282025_CHRQ2_UpdatedMainGUI/compprog1282025 directory

--- dev-shane/GUI/GUI_option2/compprog1282025_CHRQ2_UpdatedMainGUI/compprog1282025/src/main/java/com/compprog1282025/service/AuthService.java
+++
@@ -1,6 +0,0 @@
-package com.compprog1282025.service;
-
-public interface AuthService {
-    boolean authenticateEmployee(int empNumber, String password);
-    boolean authenticateAdmin(String username, String password);
-}
